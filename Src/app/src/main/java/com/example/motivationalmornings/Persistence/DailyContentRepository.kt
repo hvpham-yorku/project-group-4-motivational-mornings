897573d8da1e1336f06a2fb79f8725102ff5b828
JordiNakahara <jordi13@my.yorku.ca>
2026-03-04 00:13:27 -0500

    Fix an error and completed insert functions for intentions and quotes of the day.

--- Src/app/src/main/java/com/example/motivationalmornings/Persistence/DailyContentRepository.kt
+++ Src/app/src/main/java/com/example/motivationalmornings/Persistence/DailyContentRepository.kt
@@ -2,6 +2,7 @@ package com.example.motivationalmornings.Persistence
 import androidx.room.ColumnInfo
 import androidx.room.Dao
 import androidx.room.Entity;
+import androidx.room.Insert
 import androidx.room.PrimaryKey;
 import androidx.room.Query
 
@@ -11,7 +12,7 @@ data class Intention(
     @ColumnInfo(name = "text") val text: String,
     @ColumnInfo(name = "date") val date: String,
 )
-
+@Entity
 data class QuoteOfTheDay(
     @PrimaryKey val uid: Int,
     @ColumnInfo(name = "text") val text: String,
@@ -24,4 +25,11 @@ interface DailyContentDao {
 
     @Query("SELECT * FROM QuoteOfTheDay")
     fun getAllQuotes(): List<QuoteOfTheDay>
+
+    @Insert
+    fun insertIntention(intention: Intention)
+
+    @Insert
+    fun insertQuote(quote: QuoteOfTheDay)
+
 }
